633b4f907d9da04fa483ac24e936557aa1ced897
Danny McGee <dannymcgee@gmail.com>
2021-12-07 10:44:23 -0500

    Fix missing Overlay provider error

--- packages/components/src/lib/menu/menu.module.ts
+++ packages/components/src/lib/menu/menu.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { OverlayModule } from "@angular/cdk/overlay";
 
 import { IconModule } from "@electric/components/icon";
 
@@ -17,6 +18,7 @@ import {
 	imports: [
 		CommonModule,
 		IconModule,
+		OverlayModule,
 	],
 	declarations: [
 		MenuComponent,
