73a0753271d9de10bace39df6bdda6dedb3f1b04
Danny McGee <dannymcgee@gmail.com>
2023-02-13 22:20:59 -0500

    framework: move comptime decorators to sub-module

--- packages/framework/src/index.ts
+++ packages/framework/src/index.ts
@@ -1,10 +1,1 @@
-import comptimeDecorators from "@electric/comptime-decorators"
-
-import { Component } from "./lib/decorators/comptime"
-
 export * from "./lib/decorators/runtime/"
-
-// Plugin hook for Nx custom transformers loader
-// TODO: Move this into a sub-module definition,
-// like @electric/framework/decorators/comptime
-export const before = comptimeDecorators({ Component })
--- packages/framework/src/lib/decorators/comptime/index.ts
+++ packages/framework/src/lib/decorators/comptime/index.ts
@@ -1,1 +1,6 @@
-export * from "./component"
+import comptimeDecorators from "@electric/comptime-decorators"
+
+import { Component } from "./component"
+
+// Plugin hook for Nx custom transformers loader
+export const before = comptimeDecorators({ Component })
