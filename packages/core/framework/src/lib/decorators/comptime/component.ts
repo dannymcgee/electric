78e30f07bcc8bcffa99bc856f5c81b53e48b8dea
Danny McGee <dannymcgee@gmail.com>
2023-02-14 22:20:29 -0500

    framework: add static observedAttributes[] to component

--- packages/core/framework/src/lib/decorators/comptime/component.ts
+++ packages/core/framework/src/lib/decorators/comptime/component.ts
@@ -1,9 +1,34 @@
 import { ClassDecorator } from "@electric/comptime-decorators"
+import * as ts from "typescript"
+
+import { getContext } from "./context"
 
 export function Component(tagName: string): ClassDecorator {
 	return function (node, $) {
+		const cx = getContext.call(this)
+		const observedAttributes = $.PropertyDecl(
+			[$.Token(ts.SyntaxKind.StaticKeyword)],
+			$.Identifier("observedAttributes"),
+			undefined,
+			undefined,
+			$.ArrayLiteralExpr(
+				cx.getInputs(this.original(node))
+					.map(p => $.StringLiteral((p.name as ts.Identifier).text))
+			)
+		)
+
 		return [
-			node,
+			$.updateClassDecl(
+				node,
+				node.modifiers,
+				node.name,
+				node.typeParameters,
+				node.heritageClauses,
+				[
+					observedAttributes,
+					...node.members
+				]
+			),
 			$.ExpressionStmt(
 				$.CallExpr(
 					$.PropertyAccessExpr(
