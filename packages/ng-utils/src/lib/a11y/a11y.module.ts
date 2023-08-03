01cf092ae9df99c86e788a7144226a25b1af6f00
Danny McGee <dannymcgee@gmail.com>
2023-08-02 20:19:14 -0400

    ButtonBehaviorDirective

--- packages/ng-utils/src/lib/a11y/a11y.module.ts
+++ packages/ng-utils/src/lib/a11y/a11y.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from "@angular/core";
 import { AutofocusDirective } from "./autofocus.directive";
+import { ButtonBehaviorDirective } from "./button-behavior.directive";
 import { KeybindDirective } from "./keybind.directive";
 
 @NgModule({
 	declarations: [
 		AutofocusDirective,
+		ButtonBehaviorDirective,
 		KeybindDirective,
 	],
 	exports: [
 		AutofocusDirective,
+		ButtonBehaviorDirective,
 		KeybindDirective,
 	],
 })
---
+++ packages/ng-utils/src/lib/a11y/button-behavior.directive.ts
@@ -0,0 +1,45 @@
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
+import { Directive, ElementRef, HostBinding, HostListener, Input } from "@angular/core";
+
+@Directive({
+	selector: "[role=\"button\"]",
+})
+export class ButtonBehaviorDirective {
+	@Input()
+	get disabled() { return this._disabled; }
+	set disabled(value) {
+		this._disabled = coerceBooleanProperty(value);
+		if (
+			this._element instanceof HTMLButtonElement
+			|| this._element instanceof HTMLInputElement
+		) {
+			this._element.disabled = this._disabled;
+		} else {
+			if (value) {
+				this._element.setAttribute("aria-disabled", "true");
+			} else {
+				this._element.removeAttribute("aria-disabled");
+			}
+		}
+	}
+	private _disabled?: boolean;
+
+	@HostBinding()
+	get tabIndex(): number {
+		if (this._disabled) return -1;
+		return 0;
+	}
+
+	private get _element() { return this._elementRef.nativeElement; }
+
+	constructor (
+		private _elementRef: ElementRef<HTMLElement>,
+	) {}
+
+	@HostListener("keydown.enter")
+	@HostListener("keydown.space", ["$event"])
+	_emitClick(event?: Event): void {
+		event?.preventDefault();
+		this._element?.click();
+	}
+}
