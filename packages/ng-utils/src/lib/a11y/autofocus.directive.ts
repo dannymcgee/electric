aaeaef40e8f34e29b6d32c7da07ad3cc3dfccd23
Danny McGee <dannymcgee@gmail.com>
2023-08-06 16:52:38 -0400

    Make AutofocusDirective more flexible

--- packages/ng-utils/src/lib/a11y/autofocus.directive.ts
+++ packages/ng-utils/src/lib/a11y/autofocus.directive.ts
@@ -1,22 +1,57 @@
 import { FocusMonitor, FocusOrigin, FocusOptions } from "@angular/cdk/a11y";
-import { Directive, ElementRef } from "@angular/core";
+import {
+	Directive,
+	ElementRef,
+	Input,
+	OnDestroy,
+	OnInit,
+} from "@angular/core";
+import { Subject } from "rxjs";
 
+import { Coerce } from "../coerce";
 import { Focusable, INITIAL_FOCUS_TARGET } from "./a11y.types";
 
 @Directive({
-	selector: "[elxAutofocus]",
+	selector: "[elxAutofocus], [elxFocusTarget]",
 	providers: [{
 		provide: INITIAL_FOCUS_TARGET,
 		useExisting: AutofocusDirective,
 	}],
 })
-export class AutofocusDirective implements Focusable {
+export class AutofocusDirective implements OnInit, OnDestroy, Focusable {
+	/**
+	 * When set, the element will be focused as soon as it enters the DOM.
+	 *
+	 * Use the `[elxFocusTarget]` selector instead if this behavior is
+	 * undesirable.
+	 */
+	@Coerce(Boolean)
+	@Input("elxAutofocus") autofocus = false;
+
+	private _onDestroy$ = new Subject<void>();
+
 	constructor (
-		private _elementRef: ElementRef<HTMLElement>,
+		private _elementRef: ElementRef<HTMLOrSVGElement>,
 		private _focusMonitor: FocusMonitor,
 	) {}
 
+	ngOnInit(): void {
+		if (this.autofocus)
+			setTimeout(() => this.focus());
+	}
+
+	ngOnDestroy(): void {
+		this._onDestroy$.next();
+		this._onDestroy$.complete();
+	}
+
 	focus(origin?: FocusOrigin, options?: FocusOptions) {
-		this._focusMonitor.focusVia(this._elementRef, origin ?? null, options);
+		this._focusMonitor.focusVia(
+			// FocusMonitor expects an ElementRef<HTMLElement>, but it does
+			// actually work with ElementRef<SVGElement>
+			this._elementRef as ElementRef<HTMLElement>,
+			origin ?? "program",
+			options,
+		);
 	}
 }
