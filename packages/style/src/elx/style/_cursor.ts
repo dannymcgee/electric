ce43521008b5e257549acfe0b02cc866e5ebc33b
Danny McGee <dannymcgee@gmail.com>
2023-09-06 18:19:30 -0400

    Add pen cursor variants

--- packages/style/src/elx/style/_cursor.ts
+++ packages/style/src/elx/style/_cursor.ts
@@ -1,9 +1,14 @@
+import { Option } from "@electric/utils";
+
 export namespace cursor {
 	export function select(fallback = "default") {
 		return `url("/assets/elx/cursors/Select.svg") 3 2, ${fallback}`;
 	}
 
-	export function pen(fallback = "default") {
-		return `url("/assets/elx/cursors/Pen.svg") 3 2, ${fallback}`;
+	export function pen(variant: Option<string> = null, fallback = "default") {
+		if (!variant)
+			return `url("/assets/elx/cursors/Pen.svg") 3 2, ${fallback}`;
+
+		return `url("/assets/elx/cursors/Pen_${variant}.svg") 3 2, ${fallback}`;
 	}
 }
