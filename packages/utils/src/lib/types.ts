648806e5a2a096a88f5570d34bd9504a7f0453a3
Danny McGee <dannymcgee@gmail.com>
2023-09-06 17:45:18 -0400

    Add `Const<T>` type with C++-ish semantics

--- packages/utils/src/lib/types.ts
+++ packages/utils/src/lib/types.ts
@@ -12,3 +12,24 @@ export interface Fn<Args extends any[] = [], R = void> {
 export interface AsyncFn<Args extends any[] = [], R = void> {
 	(...args: Args): Promise<R>;
 }
+
+export interface Ctor<T, Args extends any[] = []> {
+	new (...args: Args): T;
+}
+
+export function instanceOf<T>(Type: Ctor<T, any[]>) {
+	return (inst: unknown): inst is T => inst instanceof Type;
+}
+
+export type Const<T> = {
+	readonly [K in keyof T]:
+		T[K] extends Array<any> ? ConstMappedArray<T[K]> :
+		T[K] extends Set<infer U> ? ReadonlySet<Const<U>> :
+		T[K] extends Map<infer U, infer V> ? ReadonlyMap<U, Const<V>> :
+		T[K] extends ((...args: any[]) => any) ? T[K] :
+		T[K] extends Const<any> ? T[K] :
+		T[K] extends { [key: string|number|symbol]: any } ? Const<T[K]> :
+		T[K];
+}
+
+type ConstMappedArray<T extends any[]> = readonly [...{ [Idx in keyof T]: Const<T[Idx]> }];
