a55ca0dc62144d0af0f609bf706055096948f0a7
Danny McGee <dannymcgee@gmail.com>
2025-03-15 22:36:35 -0400

    Failed attempt to debug massive input latency
    The [StackBlitz] demo that I used as the basis for a blog post about
    the realtime architecture here is implemented in Angular v16, and that's
    working fine, so maybe the issue will go away on its own with further
    updates.
    
    [StackBlitz]: https://stackblitz.com/edit/stackblitz-starters-jxv9fj?file=package.json

--- packages/ng-utils/src/lib/detect-changes.ts
+++ packages/ng-utils/src/lib/detect-changes.ts
@@ -1,7 +1,3 @@
-import { ɵdetectChanges as detectChanges } from "@angular/core";
-
-import { decorateMethod, NgClass } from "./internal/decorate";
-
 /**
  * Marks a component as dirty (needing change detection) when the property value
  * changes. This can be useful for `OnPush` components when a value is changed
@@ -12,30 +8,37 @@ import { decorateMethod, NgClass } from "./internal/decorate";
  * stable between Angular minor/patch versions. You have been warned!
  */
 export function DetectChanges(): PropertyDecorator {
-	return (proto, propName) => {
-		let propSymbol = Symbol(propName.toString());
-		let initSymbol = Symbol("init");
+	// TODO: This doesn't appear to actually be doing anything particularly
+	//       important (at least in Glyphy). Only the form controls are actually
+	//       using this decorator at all, and there aren't any of those
+	//       participating in Glyphy's canvas stuff.
+	//
+	//       Investigate where/whether this is still necessary, and find an
+	//       alternative way to do it.
+	return (_proto, _propName) => {
+		// let propSymbol = Symbol(propName.toString());
+		// let initSymbol = Symbol("init");
 
-		decorateMethod(proto as NgClass, "ngOnInit", function (this: any) {
-			this[initSymbol] = true;
-		});
+		// decorateMethod(proto as NgClass, "ngOnInit", function (this: any) {
+		// 	this[initSymbol] = true;
+		// });
 
-		Object.defineProperty(proto, propName, {
-			get() {
-				return this[propSymbol];
-			},
-			set(value: any) {
-				this[propSymbol] = value;
+		// Object.defineProperty(proto, propName, {
+		// 	get() {
+		// 		return this[propSymbol];
+		// 	},
+		// 	set(value: any) {
+		// 		this[propSymbol] = value;
 
-				if (this[initSymbol]) {
-					// FIXME: this is going to be significantly slower than the old
-					//        `markDirty` call. Instead, see if we can `inject(ChangeDetectorRef)`
-					//        from the decorator now that we have a function for that
-					detectChanges(this);
-				}
-			},
-			enumerable: true,
-			configurable: true,
-		});
+		// 		if (this[initSymbol]) {
+		// 			// FIXME: this is going to be significantly slower than the old
+		// 			//        `markDirty` call. Instead, see if we can `inject(ChangeDetectorRef)`
+		// 			//        from the decorator now that we have a function for that
+		// 			detectChanges(this);
+		// 		}
+		// 	},
+		// 	enumerable: true,
+		// 	configurable: true,
+		// });
 	}
 }
