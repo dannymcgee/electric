552fb03b0164e53f1f7bcb2a489640bc045815e0
Danny McGee <dannymcgee@gmail.com>
2025-08-23 14:52:47 -0400

    Fix Angular `track` expressions

--- apps/glyphy/src/app/glyph/glyph.ts
+++ apps/glyphy/src/app/glyph/glyph.ts
@@ -5,6 +5,14 @@ import { Path } from "./path";
 export class Glyph {
 	outline?: Const<Path>;
 
+	get trackId(): string {
+		let result = `${this.fontStyle} ${this.name}`;
+		if (this.unicode != null)
+			return `${result} 0x${this.unicode.toString(16).toUpperCase()}`;
+
+		return result;
+	}
+
 	constructor (
 		public fontStyle: string,
 		public name: string,
